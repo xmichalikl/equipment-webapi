522596b830ba55fbd3b70d25935c39c1f046019f
xmichalikl <xmichalikl@stuba.sk>
2024-05-25 10:51:14 +0200

    Added dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -35,6 +35,9 @@ switch ($command) {
     "mongo" {
         mongo up
     }
+    "docker" {
+        docker build -t xmichalikl/equipment-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
     default {
         throw "Unknown command: $command"
     }
